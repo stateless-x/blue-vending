c7574335bb1668a64af0a4ac96b71df5ca706a41
xwanats <b.wanats@gmail.com>
2023-09-01 17:44:27 +0700

    disable purchase for product that run out of stock

--- frontend/blue-vending/src/components/Product.jsx
+++ frontend/blue-vending/src/components/Product.jsx
@@ -1,16 +1,20 @@
 import "../styles/product.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PaymentUI } from "../components/PaymentUI";
 
 export const Product = ({ product, vendingMachineId }) => {
   const [showDialog, setShowDialog] = useState(false);
   const [showPaymentUI, setShowPaymentUI] = useState(false);
-
+  const [stock, setStock] = useState(product.stock);
   const handleSelect = () => {
     setShowDialog(true);
     setShowPaymentUI(true);
     document.body.classList.add("no-scroll");
   };
+
+  useEffect(() => {
+    setStock(product.stock);
+  }, [product.stock]);
 
   const closeDialog = () => {
     setShowDialog(false);
@@ -24,6 +28,7 @@ export const Product = ({ product, vendingMachineId }) => {
 
   return (
     <div className={"product-container"}>
+      {stock <= 0 && <div className="out-of-stock-overlay">Out of Stock</div>}
       <img
         className="product-image"
         src={product.productImage}
@@ -33,7 +38,12 @@ export const Product = ({ product, vendingMachineId }) => {
         <h3>{product.productName}</h3>
         <h3>{product.price} THB</h3>
       </div>
-      <button className="select-product" type="button" onClick={handleSelect}>
+      <button
+        className={`select-product ${stock <= 0 ? "disabled-button" : ""}`}
+        type="button"
+        onClick={handleSelect}
+        disabled={stock <= 0 ? "true" : ""}
+      >
         SELECT ITEM
       </button>
       {showDialog && (
