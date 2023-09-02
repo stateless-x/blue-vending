9764c3631e52fc50e2a31a706e28c47db9c8f1f6
xwanats <b.wanats@gmail.com>
2023-09-03 03:14:47 +0700

    remove hardcode for db

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 const Sequelize = require("sequelize");
 
-const db_name = "bluepi"
-const username = "postgres"
-const password = "qwerty123"
+const dbName = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const dialect = process.env.DB_DIALECT;
 
-const sequelize = new Sequelize(db_name, username, password, {
-    host: "localhost",
-    dialect: "postgres",
+const sequelize = new Sequelize(dbName, username, password, {
+    host: host,
+    dialect: dialect,
 });
 
 module.exports = sequelize;
